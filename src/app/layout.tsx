caec710a09ca713fbb741d7ff78c7690251115be
Etheral <etheralgit1@gmail.com>
2024-06-22 10:09:32 +0700

    anti

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { motion } from "framer-motion";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${montserrat.className} mx-auto max-w-[105rem]  md:px-4 `}
+      >
+        <img
+          src="/spline.png"
+          alt=""
+          className="absolute right-0 top-[-30rem] left-0 w-full z-[-1] opacity-80 animate-move-down lg:block hidden"
+        />
+        <img
+          src="/spline.png"
+          alt=""
+          className="absolute right-0 top-0 left-0 w-full z-[-1] opacity-80 lg:hidden block"
+        />
+
+        <div className="absolute z-[-11] top-4 right-2 w-[20%] h-[30%] rounded-full green_gradient" />
+        <div className="absolute z-[-11] top-4 left-2 w-[20%] h-[30%] rounded-full green_gradient" />
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +14,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#3DFFC4",
+        dark: "#1e1e1e",
+      },
+      keyframes: {
+        moveDown: {
+          "0%": { top: "-30rem", opacity: "0" },
+          "100%": { top: "0rem" },
+        },
+      },
+      animation: {
+        "move-down": "moveDown 0.8s ease-in-out 3.3s forwards",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
 export default config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
